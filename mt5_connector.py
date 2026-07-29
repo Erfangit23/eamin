5c77eedb4a62c963861fd5a6bfda22fc7bb3ed13
erfan <erfangetimage@gmail.com>
2026-07-29 13:55:55 +0330

    Fix missing comma after comment field (syntax error)

--- mt5_connector.py
+++ mt5_connector.py
@@ -213,7 +213,7 @@ class MT5Connector:
             "tp": tp_price_norm,
             "deviation": 20,
             "magic": 779900,
-            "comment": f"{signal.source_channel.lstrip('@')}|XAU"
+            "comment": f"{signal.source_channel.lstrip('@')}|XAU",
             "type_time": mt5.ORDER_TIME_GTC,
             "type_filling": filling_type,
         }
